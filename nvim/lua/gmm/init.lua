23530032306bfdc51a3638a7ce1b42af06a03420
Gareth McNicol <1968845+gmcnicol@users.noreply.github.com>
2023-12-03 11:12:44 +0000

    Initial Commit...

---
+++ nvim/lua/gmm/init.lua
@@ -0,0 +1,3 @@
+require("gmm.remap")
+require("gmm.lazy")
+
---
+++ nvim/lua/gmm/lazy.lua
@@ -0,0 +1,34 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function ()
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+	},
+	{"nvim-treesitter/playground"},
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
+})

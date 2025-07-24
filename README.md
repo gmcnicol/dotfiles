ad76b772738a148f1ab20faca6fca289c761d80f
Gareth McNicol <1968845+gmcnicol@users.noreply.github.com>
2025-07-24 13:12:11 +0100

    Add Neovim plugin setup (#1)
    * feat(nvim): expand plugin setup
    
    * docs: add plugin command cheatsheet

--- README.md
+++ README.md
@@ -31,3 +31,47 @@ sudo apt-get install inotify-tools
 # macOS (with Homebrew)
 brew install inotify-tools
 ```
+
+## Neovim setup
+
+These dotfiles include a minimal [lazy.nvim](https://github.com/folke/lazy.nvim) configuration.  The plugin list focuses on Java development and general quality-of-life enhancements.
+
+### Included plugins
+
+- `mason.nvim`, `mason-lspconfig.nvim` and `nvim-lspconfig` for installing and configuring language servers
+- `mfussenegger/nvim-jdtls` for Java LSP support
+- `hrsh7th/nvim-cmp` with buffer, path and LuaSnip completion sources
+- `L3MON4D3/LuaSnip` and `rafamadriz/friendly-snippets` for snippets
+- `nvim-treesitter` with `nvim-treesitter-context`
+- `telescope.nvim` and `neo-tree.nvim` for navigation
+- `nvim-dap`, `nvim-dap-ui` and virtual text (plus the Java debug adapter) for debugging
+- `neotest` with the JUnit adapter
+- `null-ls.nvim` with formatters and linters like `google-java-format` and Checkstyle
+- `harpoon` v2 and `git-worktree.nvim`
+- `gitsigns.nvim`, `vim-commentary` and `vim-sleuth`
+
+Most of these rely on `nvim-lua/plenary.nvim`, which is included automatically.
+
+### Plugin cheat sheet
+
+| Plugin | Sample commands |
+| ------ | --------------- |
+| **lazy.nvim** | `:Lazy` opens the plugin manager |
+| **mason.nvim** | `:Mason` shows LSP/DAP installer |
+| **nvim-lspconfig** | `:LspInfo` shows active servers |
+| **nvim-jdtls** | `:JdtCompile` / `:JdtUpdateConfig` for Java projects |
+| **nvim-cmp** | `<C-n>/<C-p>` navigate completion menu |
+| **LuaSnip** | `<C-k>` expand or jump in a snippet |
+| **telescope.nvim** | `:Telescope find_files` or `:Telescope live_grep` |
+| **neo-tree.nvim** | `:Neotree toggle` file explorer |
+| **harpoon** | `<leader>a` add file, `<leader>e` menu, `<leader>1-4` navigate |
+| **git-worktree.nvim** | `:lua require('git-worktree').create_worktree()` |
+| **gitsigns.nvim** | `:Gitsigns preview_hunk`, `:Gitsigns blame_line` |
+| **vim-commentary** | `gcc` comment line, `gc` in visual mode |
+| **nvim-treesitter** | `:TSUpdate`, `:TSInstall` parsers |
+| **nvim-treesitter-context** | `:TSContextToggle` shows code context |
+| **nvim-dap** | `:lua require('dap').continue()` etc. |
+| **nvim-dap-ui** | `:lua require('dapui').toggle()` |
+| **nvim-dap-virtual-text** | `:DapVirtualTextToggle` |
+| **neotest** | `:lua require('neotest').run.run()` |
+| **null-ls.nvim** | `:NullLsInfo` shows attached sources |
--- nvim/lua/gmm/lazy.lua
+++ nvim/lua/gmm/lazy.lua
@@ -12,28 +12,73 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function ()
-			vim.cmd([[colorscheme tokyonight]])
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
-	},
-	{
-		"christoomey/vim-tmux-navigator", 
-		lazy=false
-	},
+  -- Appearance
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
 
-	{"nvim-treesitter/playground"},
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+  -- Quality of life
+  { "tpope/vim-sleuth" },
+  { "tpope/vim-commentary" },
+  { "lewis6991/gitsigns.nvim", opts = {} },
+
+  -- Harpoon / worktree
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "ThePrimeagen/git-worktree.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  -- Treesitter
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-treesitter/nvim-treesitter-context" },
+
+  -- Fuzzy finding and file explorer
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+  },
+
+  -- LSP & completion
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "neovim/nvim-lspconfig" },
+  { "mfussenegger/nvim-jdtls" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "saadparwaiz1/cmp_luasnip" },
+
+  -- Snippets
+  { "L3MON4D3/LuaSnip" },
+  { "rafamadriz/friendly-snippets" },
+
+  -- Debugging
+  { "mfussenegger/nvim-dap" },
+  { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
+  { "theHamsta/nvim-dap-virtual-text" },
+
+  -- Testing
+  { "nvim-neotest/neotest", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-neotest/neotest-junit" },
+
+  -- Formatting and linting
+  { "jose-elias-alvarez/null-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 })

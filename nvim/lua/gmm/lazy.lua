52dfbb923b6e7898f172b8ef4df4360db5202b24
Gareth McNicol <1968845+gmcnicol@users.noreply.github.com>
2025-07-25 14:35:11 +0100

    disable neo-tree plugin

--- nvim/lua/gmm/lazy.lua
+++ nvim/lua/gmm/lazy.lua
@@ -50,6 +50,7 @@ require("lazy").setup({
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    enabled = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
